860ebff0965124e2376ce174e0792c0dff76d225
laker <laker9915@mail.ru>
2023-03-10 12:51:42 +0300

    Broke Hibernate

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -1,6 +1,7 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -15,10 +16,10 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      userService.add(new User("User1", "Lastname1", "user1@mail.ru", new Car("Lada", 12)));
+      userService.add(new User("User2", "Lastname2", "user2@mail.ru", new Car("Mercedes", 666)));
+      userService.add(new User("User3", "Lastname3", "user3@mail.ru", new Car("Lamborghini", 777)));
+      userService.add(new User("User4", "Lastname4", "user4@mail.ru", new Car("KAMAZ", 999)));
 
       List<User> users = userService.listUsers();
       for (User user : users) {
@@ -30,5 +31,7 @@ public class MainApp {
       }
 
       context.close();
+
+//      System.out.println(userService.getUserByCar("Lada", 12));
    }
 }
